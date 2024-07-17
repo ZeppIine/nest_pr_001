6d92dd7832b375db422080cb86546029ae44d639
ZeppIine <kim0904min@gmail.com>
2024-07-17 23:08:56 +0900

    ssl 임시 차단

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { HttpPrFilter } from './http_pr/http_pr.filter';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { SslMiddlePrMiddleware } from './ssl_middle_pr/ssl_middle_pr.middleware';
+// import { SslMiddlePrMiddleware } from './ssl_middle_pr/ssl_middle_pr.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  process.env.NODE_ENV == 'production'
-    ? app.use(new SslMiddlePrMiddleware().use)
-    : undefined;
+  // process.env.NODE_ENV == 'production'
+  //   ? app.use(new SslMiddlePrMiddleware().use)
+  //   : undefined;
   app.use(cookieParser());
   app.useGlobalFilters(new HttpPrFilter());
   app.useGlobalPipes(
